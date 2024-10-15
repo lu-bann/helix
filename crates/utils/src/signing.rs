51570024cd56cb060335d19984e460dd8208b855
Bob Liu <akagi201@gmail.com>
2024-10-15 21:12:03 +0800

    chore: log

--- crates/utils/src/signing.rs
+++ crates/utils/src/signing.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 use ethereum_consensus::{
     crypto::SecretKey,
     deneb::compute_fork_data_root,
@@ -9,10 +11,11 @@ use ethereum_consensus::{
     state_transition::Context,
     Error, Fork,
 };
+use tracing::info;
 
 pub const COMMIT_BOOST_DOMAIN: [u8; 4] = [109, 109, 111, 67];
 
-pub fn verify_signed_consensus_message<T: HashTreeRoot>(
+pub fn verify_signed_consensus_message<T: HashTreeRoot + Debug>(
     message: &mut T,
     signature: &BlsSignature,
     public_key: &BlsPublicKey,
@@ -42,7 +45,7 @@ pub fn verify_signed_builder_message<T: HashTreeRoot>(
     Ok(())
 }
 
-pub fn verify_signed_commit_boost_message<T: HashTreeRoot>(
+pub fn verify_signed_commit_boost_message<T: HashTreeRoot + Debug>(
     message: &mut T,
     signature: &BlsSignature,
     public_key: &BlsPublicKey,
@@ -52,6 +55,10 @@ pub fn verify_signed_commit_boost_message<T: HashTreeRoot>(
 ) -> Result<(), Error> {
     let fork_version = slot_hint.map(|slot| context.fork_version_for(context.fork_for(slot)));
     let domain = compute_commit_boost_domain(fork_version, root_hint, context)?;
+    info!(
+        "message: {:?}, signature: {:?}, public_key: {:?}, slot: {:?}, root: {:?}, fork_version: {:?}, domain: {:?}",
+        message, signature, public_key, slot_hint, root_hint, fork_version, domain
+    );
     verify_signed_data(message, signature, public_key, domain)?;
     Ok(())
 }
@@ -102,4 +109,35 @@ pub fn compute_custom_domain(
     domain[..4].copy_from_slice(domain_mask);
     domain[4..].copy_from_slice(&fork_data_root[..28]);
     Ok(domain)
+}
+
+#[cfg(test)]
+mod tests {
+    use helix_common::api::constraints_api::PreconferElection;
+
+    use super::*;
+    // write a test for verify_signed_commit_boost_message
+    // use the following inputs:
+    // {"message":{"preconfer_pubkey":"0xa7c828460fc5c8d24c60f9f30c8836659b60a610fe8b87b26a71e9b765a9d0cae16b1a963f65b3b7abe264cda187c113","slot_number":835704,"chain_id":7014190335,"gas_limit":0},"signature":"0xa6a7607926bb8d2ed4aec913bedd9d26a6b0ce05d4d8c3a887ad7114e315dacd5b5be991f3ed0f795c838b58b75ada4906bc81ece161eb8f5a8516f60560b029f47cfe9f2f7bc3205b75a7857fa87f566de7b8389f41f96791260e51ea066672"}
+    const PRECONFER_PUBKEY: &str = "0xa7c828460fc5c8d24c60f9f30c8836659b60a610fe8b87b26a71e9b765a9d0cae16b1a963f65b3b7abe264cda187c113";
+    const SLOT_NUMBER: u64 = 835704;
+    const CHAIN_ID: u64 = 7014190335;
+    const GAS_LIMIT: u64 = 0;
+    const SIGNATURE: &str = "0xa6a7607926bb8d2ed4aec913bedd9d26a6b0ce05d4d8c3a887ad7114e315dacd5b5be991f3ed0f795c838b58b75ada4906bc81ece161eb8f5a8516f60560b029f47cfe9f2f7bc3205b75a7857fa87f566de7b8389f41f96791260e51ea066672";
+    const VALIDATOR_PUBKEY: &str = "0xa7c828460fc5c8d24c60f9f30c8836659b60a610fe8b87b26a71e9b765a9d0cae16b1a963f65b3b7abe264cda187c113";
+    #[test]
+    fn test_verify_cb_signature() {
+        // use the following inputs:
+        let message = PreconferElection {
+            preconfer_pubkey: BlsPublicKey::try_from(PRECONFER_PUBKEY.as_bytes()).unwrap(),
+            slot_number: SLOT_NUMBER,
+            chain_id: CHAIN_ID,
+            gas_limit: GAS_LIMIT,
+        };
+        let signature = BlsSignature::try_from(SIGNATURE.as_bytes()).unwrap();
+        let public_key = BlsPublicKey::try_from(VALIDATOR_PUBKEY.as_bytes()).unwrap();
+        let result = verify_signed_data(&message, &signature, &BlsPublicKey::from(public_key), Domain::default());
+        // assert the result
+        assert!(result.is_ok());
+    }
 }
