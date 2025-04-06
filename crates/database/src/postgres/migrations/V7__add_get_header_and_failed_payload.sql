02f0e82785265118078f069d66ea558e78b407c8
chirag-bgh <chirag2bsingh@gmail.com>
2025-04-06 09:16:59 +0530

    fix: add mev_boost column to get_header table

--- crates/database/src/postgres/migrations/V7__add_get_header_and_failed_payload.sql
+++ crates/database/src/postgres/migrations/V7__add_get_header_and_failed_payload.sql
@@ -12,7 +12,7 @@ CREATE TABLE "get_header" (
     "parent_hash" bytea,
     "proposer_pubkey" bytea,
     "block_hash" bytea,
-    "created_at" timestamptz DEFAULT (now())
+    "mev_boost" boolean)
 );
 
 CREATE TABLE "get_header_trace" (
