4efb07381b3a48b9b65b41fd36a29913e8cd7f9b
interstate-dev <external@interstate.so>
2025-05-08 16:06:06 +0000

    added: delegate key in get_header

--- crates/common/src/api/mod.rs
+++ crates/common/src/api/mod.rs
@@ -20,7 +20,7 @@ pub const PATH_PROPOSER_API: &str = "/eth/v1/builder";
 
 pub const PATH_STATUS: &str = "/status";
 pub const PATH_REGISTER_VALIDATORS: &str = "/validators";
-pub const PATH_GET_HEADER: &str = "/header/{slot}/{parent_hash}/{pubkey}";
+pub const PATH_GET_HEADER: &str = "/header/{slot}/{parent_hash}/{pubkey}/{delegatee_key}";
 pub const PATH_GET_PAYLOAD: &str = "/blinded_blocks";
 pub const PATH_GET_HEADER_WITH_PROOFS: &str = "/header_with_proofs/{slot}/{parent_hash}/{pubkey}/{delegatee_key}";
 
